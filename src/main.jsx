96e34f20ebc663913cc0d3f794373e9c284f2a30
Amora5113 <amora@herosotware.mx>
2026-05-06 20:13:26 -0700

    modifique el main.jsx y cambie por mayusculas en el link de repo en gh

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './assets/background.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode basename="/cavanahomepageworking">
+  <StrictMode /*basename="/cavanahomepageworking"*/>
     <App />
   </StrictMode>,
 )
